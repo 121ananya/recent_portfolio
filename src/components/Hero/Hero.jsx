42c04516643d592e720cf3594d6b323d464234a6
121ananya <sinhaananya050@gmail.com>
2024-03-13 17:04:22 +0530

    change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,10 +9,10 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ananya Sinha</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 3 years of experience using Angular, React and
+          I'm a full-stack developer with almost 3 years of experience using Angular, React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:sinhaananya050@email.com" className={styles.contactBtn}>
+        <a href="https://drive.google.com/file/d/1_5H1kBMch7YnpWC43EmT_LPdTrhCsW5s/view?usp=drive_link" className={styles.contactBtn}>
           Reach Me Through
         </a>
       </div>
